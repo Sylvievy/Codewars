1e694347ff607e12446eddb58d03b3765f01a9f3
Devarshini <118905256+Sylvievy@users.noreply.github.com>
2025-01-22 22:43:02 +0530

    added negative.js

--- Javascript/negative.js
+++ Javascript/negative.js
@@ -1,6 +1,17 @@
+/*
+make a given number negative
+
+8kyu
+*/
+
 function makeNegative(num) {
     if (num>0){
       return num-(num+num)
     }
     return num
   }
+
+/*
+function makeNegative(num) {
+  return num > 0 ? -num : num;
+*/
